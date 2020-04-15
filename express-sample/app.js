66072e1d01d5a009bac000104608d7ac2a590b1a
kimyongkuk <k0603156@gmail.com>
2020-04-15 19:23:22 +0900

    Check Operation, Rename Dir, Init angular-sample

--- express-sample/app.js
+++ express-sample/app.js
@@ -16,12 +16,12 @@ app.use("/post", PostRouter);
 app.use("/user", UserRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // res.locals.message = err.message;
   // res.locals.error = req.app.get("env") === "development" ? err : {};
   // res.status(err.status || 500);
