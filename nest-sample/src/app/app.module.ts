4582572f6514f51732aff8b8e9f7b38125699f6e
kimyongkuk <k0603156@gmail.com>
2020-04-15 18:49:40 +0900

    nest sampleapp basic interface controller module service

--- nest-sample/src/app/app.module.ts
+++ nest-sample/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { CatsModule } from '../cats/cats.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- nest-sample/src/main.ts
+++ nest-sample/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
   await app.listen(3000);
 }
 bootstrap();
